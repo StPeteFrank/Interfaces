74d8652cff1f95238682957789391df4eb2aa7a5
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-03-13 12:01:07 -0400

    Have my Animal classes.

---
+++ Interfaces/Classes/Bird.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces.Classes
+{
+    class Bird
+    {
+        public string Color { get; set;  }
+        public int WingSpan { get; set; }
+        public int FlySpeed { get; set; }
+    }
+}
---
+++ Interfaces/Classes/Horse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces.Classes
+{
+    class Horse
+    {
+        public int NumberOfShoes { get; set; }
+        public string ManeColor { get; set; }
+        public string Breed { get; set; }
+    }
+}
